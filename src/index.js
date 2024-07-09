7ef15fef2b212bdec357c37cf25d1dc7d0e78ae4
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-07-09 17:27:55 +0200

    Adds methods to linked list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import linkedList from "./linkedList";
 
-window.myList = new linkedList();
+let newList = new linkedList();
+newList.append("1");
+newList.append("2");
+newList.append("3");
+
+window.myList = newList;
--- src/linkedList.js
+++ src/linkedList.js
@@ -16,4 +16,125 @@ export default class linkedList {
       lastNode.nextNode = new Node(value);
     }
   }
+
+  prepend(value) {
+    this.head = new Node(value, this.head);
+  }
+
+  getSize() {
+    let size = 0;
+    let lastNode = this.head;
+    while (lastNode !== null) {
+      size++;
+      lastNode = lastNode.nextNode;
+    }
+    return size;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  getTail() {
+    let lastNode = this.head;
+    while (lastNode.nextNode !== null) {
+      lastNode = lastNode.nextNode;
+    }
+    return lastNode;
+  }
+
+  at(index) {
+    let searchedNode = this.head;
+    for (let i = 0; i < index; i++) {
+      searchedNode = searchedNode.nextNode;
+      if (searchedNode === null) {
+        console.log("No node found at this index");
+        return;
+      }
+    }
+    return searchedNode;
+  }
+
+  pop() {
+    if (this.head !== null) {
+      let secToLastNode = this.head;
+      if (secToLastNode.nextNode === null) {
+        this.head = null;
+      } else {
+        while (secToLastNode.nextNode.nextNode !== null) {
+          secToLastNode = secToLastNode.nextNode;
+        }
+        secToLastNode.nextNode = null;
+      }
+    }
+  }
+
+  contains(value) {
+    let nodeSearch = this.head;
+    while (nodeSearch !== null) {
+      if (nodeSearch.value.includes(value)) {
+        return true;
+      } else {
+        nodeSearch = nodeSearch.nextNode;
+      }
+    }
+    return false;
+  }
+
+  find(value) {
+    let nodeSearch = this.head;
+    let index = 0;
+    while (nodeSearch !== null) {
+      if (nodeSearch.value.includes(value)) {
+        return index;
+      } else {
+        nodeSearch = nodeSearch.nextNode;
+        index++;
+      }
+    }
+    return null;
+  }
+
+  toString() {
+    let node = this.head;
+    let printList = "";
+    while (node !== null) {
+      printList += `( ${node.value} ) -> `;
+      node = node.nextNode;
+    }
+    printList += "null";
+    console.log(printList);
+  }
+
+  insertAt(value, index) {
+    if (index === 0) {
+      this.head = new Node(value, this.head);
+    } else {
+      let node = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        node = node.nextNode;
+      }
+      node.nextNode = new Node(value, node.nextNode);
+    }
+  }
+
+  removeAt(index) {
+    if (this.head !== null) {
+      if (index === 0) {
+        this.head = this.head.nextNode;
+      } else {
+        let node = this.head;
+        for (let i = 0; i < index - 1; i++) {
+          node = node.nextNode;
+          if (node === null || node.nextNode === null) {
+            console.log("No node found at this index");
+            return;
+          }
+        }
+        node.nextNode = node.nextNode.nextNode;
+      }
+    } else {
+      console.log("No node found at this index");
+    }
+  }
 }
